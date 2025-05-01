079b6d973def9d5ff2fff7bda7f08ae621914ca7
vishnu <vishnuthambankv@gmail.com>
2025-05-01 22:25:31 +0530

    middleware corrected

--- backend/middlewares/authDoctor.js
+++ backend/middlewares/authDoctor.js
@@ -9,6 +9,7 @@ const authDoctor = async (req, res, next) => {
         }
         const token_decode = jwt.verify(dtoken, process.env.JWT_SECRET)
 
+        req.body = req.body || {};
         req.body.docId = token_decode.id
 
         next()
